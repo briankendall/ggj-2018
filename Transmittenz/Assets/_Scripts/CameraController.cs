9c9293a45a4ce12123042146993536ad9acdcf29
Brian Kendall <brian@briankendall.net>
2018-01-27 12:11:44 -0500

    Added some more crap, I dunno! There's a red herring image now

---
+++ Transmittenz/Assets/_Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    
+    BoxCollider2D playerBoxCollider;
+    
+	// Use this for initialization
+	void Start () {
+		playerBoxCollider = player.GetComponent<BoxCollider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.localPosition = new Vector3(playerBoxCollider.bounds.center.x, playerBoxCollider.bounds.center.y,
+                                              transform.localPosition.z);
+	}
+}
